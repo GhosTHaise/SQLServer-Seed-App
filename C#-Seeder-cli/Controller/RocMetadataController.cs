694faaa4d8252306b3d2ca83b2911fc1e4fe101f
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-02-17 21:08:45 +0300

    feat : metadata - algo

---
+++ C#-Seeder-cli/Controller/RocMetadataController.cs
@@ -0,0 +1,72 @@
+﻿using C__Seeder_cli.Database;
+
+namespace Controller
+{
+    public class RocMetadataController
+    {
+        private readonly WebHelpRocContext context;
+        public RocMetadataController(WebHelpRocContext _context)
+        {   
+            context = _context;
+        }
+
+        public static String[] BaseName = {"DE","FR","EN","IT","SP","CAMP"};
+        public static String[] Name = {"LINKY","NIXIS","TDE_WINBACK","TEST","SPARK","EMAILING"};
+        
+        public  void GenerateRocCampaign()
+        {
+            Console.WriteLine("Please enter the number of Day to be generated:");
+            //int userInput = int.Parse(Console.ReadLine() ?? "0");
+            String[] PF = context.RocPfs.Select( cc => cc.RocPfname ).ToArray<string>();
+            int? maxProcessIdStored = context.RocMetadata.Max(m => m.RocRunProcessId) ?? null;
+            if(maxProcessIdStored != null){
+                DateTime? lastDataTime = context.RocMetadata.Max(m => m.RocDateRunProcess);
+                if(lastDataTime != null){
+                    TimeSpan difference = DateTime.Today - (lastDataTime ?? DateTime.Today);
+                    int differenceJour = difference.Days;
+                }
+            }else{
+
+            }
+
+            /* if (1 > 0 || true)
+            {
+
+                for (int i = 0; i < userInput; i++)
+                {
+                    NewRocCampaign(context, i,PF);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid number.");
+            } */
+        }
+
+         private static void RunProcess(int ProcessID,int day_difference){
+            
+            for(int i=1;i <= day_difference;i++){
+
+            }
+         }
+
+        private static void NewRocCampaign(WebHelpRocContext context, int i,string[] PfName)
+        {
+            RocMetadatum _temp = new RocMetadatum
+            {
+               
+            };
+
+            try
+            {
+                context.RocMetadata.Add(_temp);
+                context.SaveChanges(); // Save changes to the database
+                Console.WriteLine($"[{i}] : Campaign created.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ C#-Seeder-cli/Utils/FileDayInterval.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace utils
+{
+    public class FileDayInterval
+    {
+        int max;
+        int min;
+
+        public FileDayInterval(int _min , int _max)
+        {
+            this.max = _max;
+            this.min = _min;
+        }
+    
+
+
+        public Dictionary<int,FileDayInterval> getDaysFileMigrationInterval(){
+            Dictionary<int, FileDayInterval> dc = new Dictionary<int, FileDayInterval>
+            {
+                { 0, new FileDayInterval(271, 1200) },
+                { 1, new FileDayInterval(80234,92000)},
+                { 2, new FileDayInterval(92234,110120)},
+                { 3, new FileDayInterval(79934,84960)},
+                { 4, new FileDayInterval(78234,85320)},
+                { 5, new FileDayInterval(83234,86780)},
+                { 6, new FileDayInterval(5234,7780)},
+
+            };
+
+            return dc;
+        }
+    }
+}
